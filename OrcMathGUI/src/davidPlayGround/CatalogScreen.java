6e4a9ef60aee70f8c3ee6a628eb2822fa2d33e0f
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-12-20 11:43:17 -0500

    asdf

--- OrcMathGUI/src/davidPlayGround/CatalogScreen.java
+++ OrcMathGUI/src/davidPlayGround/CatalogScreen.java
@@ -1,26 +1,79 @@
 package davidPlayGround;
 
+import java.io.File;
 import java.util.List;
 
-import guiTeacher.components.RadioButton;
-import guiTeacher.components.TextField;
+import javax.swing.JFrame;
+
+import guiTeacher.components.*;
+import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class CatalogScreen extends FullFunctionScreen {
+public class CatalogScreen extends FullFunctionScreen implements FileRequester {
 
 	private static final long serialVersionID = 258186143576427947L;
 	private TextField field;
 	private TextField Title;
 	private TextField Author;
 	private TextField pages;
+	private TextArea text;
+	private Button addButton;
+	private Button saveButton;
+	private Button deleteButton;
+	private FileOpenButton openButton;
+	private Button loadButton;
+	private CharacterMaker catalog;
 	
 	public CatalogScreen(int width, int height) {
 		super(width, height);
 	}
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
+		catalog = new CharacterMaker();
+		
+		openButton = new FileOpenButton(600, 40, 40, 40, null, this);
+		saveButton = new Button(700, 40, 40, 40, "Save", new Action() {
+			
+			@Override
+			public void act() {
+				saveClicked();
+				
+			}
+		});
+		addButton = new Button(650, 40, 40, 40, "Add", new Action() {
+			
+			@Override
+			public void act() {
+				addClicked();
+				
+			}
+		} );
+		deleteButton = new Button(540, 40, 50, 40, "Delete", new Action() {
+			
+			@Override
+			public void act() {
+				deleteClicked();
+				
+			}
+		});
+		loadButton = new Button(500, 40, 40, 40, "Load", new Action() {
+			
+			@Override
+			public void act() {
+				loadClicked();
+				
+			}
+		});
+		text = new TextArea(100, 100, 100, 70, "This is where text goes");
+		viewObjects.add(loadButton);
+		viewObjects.add(text);
+		viewObjects.add(deleteButton);
+		viewObjects.add(openButton);
+		viewObjects.add(addButton);
+		viewObjects.add(saveButton);
 		field = new TextField(40, 40, 200, 30, "Text goes here", "Description");
+		field.setInputType(field.INPUT_TYPE_NUMERIC);
 		RadioButton rb1 = new RadioButton(300, 40, 30, 30, "X", null);
 		RadioButton rb2 = new RadioButton(400, 40, 30, 30, "Y", null);
 		rb1.addPeer(rb2);
@@ -28,6 +81,33 @@ public class CatalogScreen extends FullFunctionScreen {
 		viewObjects.add(rb1);
 		viewObjects.add(rb2);
 		viewObjects.add(field);
+	}
+	protected void loadClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+	protected void deleteClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+	protected void saveClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+	protected void addClicked() {
+		Animation update = new Animation("name", "color", 17, "place");
+		text.setText(update.toString());
+		
+	}
+	@Override
+	public void setFile(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public JFrame getWindow() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- OrcMathGUI/src/davidPlayGround/CharacterMaker.java
+++ OrcMathGUI/src/davidPlayGround/CharacterMaker.java
@@ -1,6 +1,9 @@
 package davidPlayGround;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CharacterMaker {
 
@@ -12,7 +15,40 @@ public class CharacterMaker {
 		newCharacters.add(new Animation("SuperGodDavid", "green", 17, "resources/characterC"));
 	}
 	public static void main(String[] args) {
-		
+		CharacterMaker maker = new CharacterMaker();
+		maker.addCharacter();
+		System.out.println(maker.getCSVContent());
+	}
+	public void addCharacter(){
+		Scanner in = new Scanner(System.in);
+	//	System.out.println("Would you like to load a file?");
+	//	if (in.nextLine().equals("Yes")) {
+	//		
+	//	}
+		System.out.println("What is your name?");
+	 	String namez = in.nextLine();
+		System.out.println("What color are your eyes?");
+		String colorz = in.nextLine();
+		System.out.println("How old are you?");
+		int agez = in.nextInt();
+		String locationz = null;
+		System.out.println("Where is your picture located?");
+		while(locationz == null || locationz.equals("")) {
+			locationz = in.nextLine();
+		}
+		Animation yup = new Animation(namez, colorz, agez, locationz);
+		System.out.println(yup);
+		newCharacters.add(yup);
+		in.close();
+		try {
+		FileWriter fw=new FileWriter("test.csv");
+		System.out.println("run");
+		 fw.write(newCharacters.toString());    
+		 fw.close();   
+		}
+		catch(IOException e){
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
+		}
 	}
 	public String getCSVContent() {
 		String data = "";
